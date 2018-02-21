ce4d36a1439ad52d17f3ac2ee75426b7a859bda4
Aj Tech Developer <techdeveloper.aj@gmail.com>
2018-02-21 10:41:25 +0530

    Initial code commit

---
+++ src/main/java/com/aj/zuulservice/ZuulServiceApplication.java
@@ -0,0 +1,20 @@
+package com.aj.zuulservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulServiceApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(ZuulServiceApplication.class);
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ZuulServiceApplication.class,args);
+	}
+}
